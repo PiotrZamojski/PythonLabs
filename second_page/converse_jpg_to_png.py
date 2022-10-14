3af9aab1152b8a9d4f66d3b161506638394e2c9d
Zamojski <zamojski00@gmail.com>
2022-10-14 10:56:12 +0200

    Write funtion to converte photos

--- second_page/converse_jpg_to_png.py
+++ second_page/converse_jpg_to_png.py
@@ -1,21 +1,10 @@
 import os
 from PIL import Image
 
-def findAllJpgFIles():
-    path = os.path.realpath('C:\Users\zamoj\Desktop\python\PythonLabs')
-    for root, d_names, f_names in os.walk(path):
-        print(root, d_names, f_names)
-        for i in f_names:
-            print(i)
-
-
 def jpgToPdfConverter():
-    path = os.path.realpath('C:\Users\zamoj\Desktop\python\PythonLabs')
-    for root, d_names, f_names in os.walk(path):
-        print(root, d_names, f_names)
-        for i in f_names:
-            print(i)
+    im1 = Image.open('C:\\Users\\zamoj\\Desktop\\python\\PythonLabs\\python_photos\\first_photo.jpg')
+    im1.save('C:\\Users\\zamoj\\Desktop\\python\\PythonLabs\\python_photos\\first_photo_converter.png')
 
 
 if __name__ == '__main__':
-    findAllJpgFIles()
+   jpgToPdfConverter()
