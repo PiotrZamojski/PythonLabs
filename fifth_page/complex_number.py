dbf85ecd630584c66f407041214d1f3797ba1c71
Zamojski <zamojski00@gmail.com>
2022-10-24 17:24:03 +0200

    Change complex number

--- fifth_page/complex_number.py
+++ fifth_page/complex_number.py
@@ -3,23 +3,23 @@ class Complex:
         self.r = real
         self.i = imag
 
-    def __add__(self, o):
+    def add(self, o):
         return Complex(self.r + o.r, self.i + o.i)
 
-    def __sub__(self, o):
+    def sub(self, o):
         return Complex(self.r - o.r, self.i - o.i)
 
-    def __mul__(self, o):
+    def mul(self, o):
         return Complex(self.r * o.r - self.i * o.i, self.r * o.i + self.i * o.r)
 
-    def __truediv__(self, o):
+    def div(self, o):
         m = o.r * o.r + o.i * o.i
         return Complex((self.r * o.r + self.i * o.i)/m, (self.i * o.r - self.r * o.i)/m)
 
     def modulus(self):
         return (pow(pow(self.r,2) + pow(self.i,2) , 1/2))
         
-    def __output__(self):
+    def __str__(self):
         if self.i == 0:
             return '%.2f' % self.r
         if self.r == 0:
@@ -29,16 +29,14 @@ class Complex:
         else:
             return '%.2f + %.2fi' % (self.r, self.i)
 
-def solve(complexOne, complexTwo):
-    print(complexOne + complexTwo)
-    print(complexOne - complexTwo)
-    print(complexOne * complexTwo)
-    print(complexOne / complexTwo)
 
 if __name__ == '__main__':
     x = Complex(2, 10)
     y = Complex(3, 5)
-    solve(x,y)
+    print(x.add(y))
+    print(x.sub(y))
+    print(x.mul(y))
+    print(x.div(y))
     print(x.modulus())
     print(y.modulus())
    
