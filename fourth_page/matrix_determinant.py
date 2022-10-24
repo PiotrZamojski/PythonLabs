9e21d39f993f7795c48afd70cfd6b93aa15b56e4
Zamojski <zamojski00@gmail.com>
2022-10-24 17:29:41 +0200

    Complete determinant

--- fourth_page/matrix_determinant.py
+++ fourth_page/matrix_determinant.py
@@ -1,7 +1,5 @@
-from cgitb import reset
 from random import randint
-import re
-
+import numpy as np
 
 def newMatrix(rows, cols):
     matrix = []
@@ -13,30 +11,15 @@ def newMatrix(rows, cols):
 def generateValueMatrix(m):
     for i in range(len(m)):
         for j in range(len(m[0])):
-           m[i][j] = randint(0,4)
+           m[i][j] = randint(0,2)
     for value in m:
         print(value)
     print("\n")
 
-def matrixDeterminant(a):
-    if len(a) == 1:
-        result = a[0][0]
-        print(result)
-    elif len(a) == 2:
-        result = a[0][0]*a[1][1] - a[1][0]*a[0][1]
-        print(result) 
-    else:
-        result = 0
-        for j in range(len(a)):
-            result += pow(-1,j)*a[0][j]
-            print(type(matrixDeterminant(getMatrixMinor(a,j))))
-        print(result)
-
-def getMatrixMinor(m,j):
-    x =[row[:j] + row[j+1:] for row in (m[:0]+m[1:])]
-    return 
+def calculateDet(matrix):
+    return np.linalg.det(matrix)
 
 if __name__ == '__main__':
     a = newMatrix(3,3)
     generateValueMatrix(a)
-    matrixDeterminant(a)
+    print(calculateDet(a))
